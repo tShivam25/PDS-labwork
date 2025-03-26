60c536e49ae876996a6e18dc635c1e896bb55454
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-03-26 12:54:43 +0530

    .py

---
+++ exp1.py
@@ -0,0 +1,22 @@
+#float to int
+num=3.85
+int_num=int(num)
+print (int_num)
+print(type(int_num))
+
+
+# int to float
+a=10
+b=float(a)
+print(a)
+print(type(a))
+
+#number to string
+num=100
+a=str(num)
+print(a)
+print(type(a))
+
+#question
+age=int(input("how old are you ?"))
+print("in 5 years ,your age will be ",age + 5)
---
+++ turtleTree.py
@@ -0,0 +1,25 @@
+from turtle import *
+bgcolor("black")
+pensize(2)
+color("green")
+left(90)
+backward(100)
+speed(00)
+shape("turtle")
+
+def tree(i):
+    if i<10:
+        return
+    else:
+        forward(i)
+        color("orange")
+        circle(2)
+        color("blue")
+        left(30)
+        tree(3*i/4)
+        right(60)
+        tree(3*i/4)
+        left(30)
+        backward(i)
+tree(100)
+done       
